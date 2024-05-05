90fd15b293d280c41ca34731345335c43104cb0e
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2024-05-05 12:05:41 +0200

    two step process
    summarize chunks of messages then summarize the summaries

--- zulip_digest/digest.py
+++ zulip_digest/digest.py
@@ -1,12 +1,34 @@
 from io import StringIO
 import logging
 import sys
+from typing import Callable
 from gpt4all import GPT4All
 
 from zulip_digest.zulip import ZulipMessage
 
 
-def _make_prompt(
+def _chunk_texts[T](
+    elements: list[T],
+    to_str: Callable[[T], str],
+    max_words: int,
+) -> list[list[T]]:
+    chunks: list[list[T]] = []
+    chunk_word_count = 0
+    i_start = 0
+    for i, element in enumerate(elements):
+        if i == len(elements) - 1:
+            chunks.append(elements[i_start:])
+            break
+        text_word_count = len(to_str(element).split())
+        if chunk_word_count + text_word_count >= max_words:
+            chunks.append(elements[i_start:i])
+            i_start = i
+            chunk_word_count = 0
+        chunk_word_count += text_word_count
+    return chunks
+
+
+def _make_message_summary_prompt(
     stream_name: str,
     topic_name: str,
     messages: list[ZulipMessage],
@@ -29,24 +51,20 @@ def _make_prompt(
     return prompt
 
 
-def _chunk_messages(
-    messages: list[ZulipMessage],
-    max_words: int,
-) -> list[list[ZulipMessage]]:
-    chunks: list[list[ZulipMessage]] = []
-    chunk_word_count = 0
-    i_start = 0
-    for i_message, message in enumerate(messages):
-        if i_message == len(messages) - 1:
-            chunks.append(messages[i_start:])
-            break
-        message_word_count = len(message.content.split())
-        if chunk_word_count + message_word_count >= max_words:
-            chunks.append(messages[i_start:i_message])
-            i_start = i_message
-            chunk_word_count = 0
-        chunk_word_count += message_word_count
-    return chunks
+def _make_final_summary_prompt(
+    conversation_summaries: list[str],
+) -> str:
+    prompt = ""
+    prompt += "You are a journalist expert at writing comprehensive neutral summaries of conversations on social medias."
+
+    prompt += "\nI need you to re-organize these reports."
+    prompt += "\nWrite executive summary of the propositions and arguments from the following reports."
+
+    prompt += "\n".join(f"\n---\n{summary}" for summary in conversation_summaries)
+
+    prompt += "\nSummary:"
+
+    return prompt
 
 
 def summarize_messages(
@@ -56,21 +74,26 @@ def summarize_messages(
     messages: list[ZulipMessage],
     print_progress: bool = False,
 ) -> str:
-    summary = None
-    chunks = _chunk_messages(messages, max_words=400)
-    for i_chunk, message_chunk in enumerate(chunks):
+    conversation_summaries: list[str] = []
+    chunks = _chunk_texts(
+        elements=messages,
+        max_words=400,
+        to_str=lambda message: message.content,
+    )
+    for i_chunk, chunk in enumerate(chunks):
         if print_progress:
             sys.stderr.write(
-                f"Chunk {i_chunk+1}/{len(chunks)} ({len(message_chunk)} messages)\n"
+                f"Chunk {i_chunk+1}/{len(chunks)} ({len(chunk)} messages)\n"
             )
-        prompt = _make_prompt(
+            sys.stderr.flush()
+        chunk_summar_prompt = _make_message_summary_prompt(
             stream_name=stream_name,
             topic_name=topic_name,
-            messages=message_chunk,
+            messages=chunk,
         )
-        logging.debug("Prompt:\n%s", prompt)
+        logging.debug("Prompt:\n%s", chunk_summar_prompt)
         chunk_summary_generator = model.generate(
-            prompt,
+            chunk_summar_prompt,
             temp=0.5,
             streaming=True,
         )
@@ -79,12 +102,29 @@ def summarize_messages(
             chunk_summary_builder.write(chunk_summary_token)
             if print_progress:
                 sys.stderr.write(f"Tokens generated : {i_token+1}\r")
-        if summary is None:
-            summary = chunk_summary_builder.getvalue()
-        else:
-            summary += "\n"
-            summary += chunk_summary_builder.getvalue()
-        logging.debug("Summary:\n%s", summary)
+                sys.stderr.flush()
+        conversation_summary = chunk_summary_builder.getvalue()
+        conversation_summaries.append(conversation_summary)
+        logging.debug(
+            "Summary:\n%s",
+        )
 
-    assert summary is not None
+    summary_prompt = _make_final_summary_prompt(
+        conversation_summaries=conversation_summaries,
+    )
+    logging.debug("Prompt:\n%s", summary_prompt)
+    summary_generator = model.generate(
+        prompt=summary_prompt,
+        temp=0.5,
+        max_tokens=512,
+        streaming=True,
+    )
+    summary_builder = StringIO()
+    for i_token, summary_token in enumerate(summary_generator):
+        summary_builder.write(summary_token)
+        if print_progress:
+            sys.stderr.write(f"Tokens generated : {i_token+1}\r")
+            sys.stderr.flush()
+    summary = summary_builder.getvalue()
+
     return summary
